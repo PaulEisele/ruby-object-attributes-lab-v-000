970e65ad1b51049f14661ccb86523e82eb6025c6
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-08 14:42:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
- 
+
   def initialize(name)
     @name = name
     @job = job
   end
- 
+
   def name=(name)
     name = name.split
     name = name
   end
- 
+
   def name
     "#{@_name}".strip
   end
