c694c167a30e372b5324b49456ed16aeec1a1269
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-08 14:48:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
+class Dog
 
+  def initialize
+    @name = name
+    @breed = breed
+  end
+
+  def name(name)
+    name = name
+  end
+
+  def name
+    "#{@name}".strip
+  end
+
+  def breed=(breed)
+    breed = breed
+  end
